08d954b6f80dbbbb2d65bea267de6265c75135f4
greensgit <rajgreensomr@gmail.com>
2024-05-07 18:27:56 +0530

    new code

---
+++ src/org/grs/test/Demo.java
@@ -0,0 +1,9 @@
+package org.grs.test;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("This is a demo class");
+	}
+
+}
---
+++ src/org/grs/test/Online.java
@@ -0,0 +1,9 @@
+package org.grs.test;
+
+public class Online {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is demo two");
+	}
+}
